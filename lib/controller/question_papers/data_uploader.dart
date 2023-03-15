b87dca20e7a319ec5e3e77b9f8881c6eed0a0c1e
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-15 18:10:17 +0530

    parsing question paper data to model class

--- lib/controller/question_papers/data_uploader.dart
+++ lib/controller/question_papers/data_uploader.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_study_app/model/question_paper_model.dart';
 import 'package:get/get.dart';
 
 class DataUploaderController extends GetxController {
@@ -20,9 +21,12 @@ class DataUploaderController extends GetxController {
         .where((path) =>
             path.startsWith("assets/DB/papers") && path.contains(".json"))
         .toList();
+    List<QuestionPaperModel> questionPaper = [];
     for (var paper in papersInAssets) {
       final stringPaperContent = await rootBundle.loadString(paper);
-      print(stringPaperContent);
+      questionPaper
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
     }
+    print('Item number ${questionPaper[0].id}');
   }
 }
