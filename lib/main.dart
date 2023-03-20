13290487886250b1e34634b2df697de701ce6a47
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-20 15:39:09 +0530

    initial app router created

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_study_app/data_uploader_screen.dart';
-import 'package:flutter_study_app/firebase_options.dart';
-import 'package:flutter_study_app/screens/introduction/introduction.dart';
-import 'package:flutter_study_app/screens/splash/splash_screen.dart';
+import 'package:flutter_study_app/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 // Future<void> main() async {
@@ -22,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: IntroductionScreen(),
+      getPages: AppRoutes.routes(),
     );
   }
 }
