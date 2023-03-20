11221cbcfef16c97e0c46c885cfa2bd39c265b30
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-20 15:09:36 +0530

    SplashScreen created

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_study_app/data_uploader_screen.dart';
 import 'package:flutter_study_app/firebase_options.dart';
+import 'package:flutter_study_app/screens/splash/splash_screen.dart';
 import 'package:get/get.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(GetMaterialApp(home: DataUploaderScreen()));
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   runApp(GetMaterialApp(home: DataUploaderScreen()));
+// }
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
 }
