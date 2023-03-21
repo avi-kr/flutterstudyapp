07491a3e8e20a203947f5a0f60528e69aa4bd731
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-21 18:31:27 +0530

    modified question card

--- lib/screens/home/question_card.dart
+++ lib/screens/home/question_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_study_app/model/question_paper_model.dart';
+import 'package:get/get.dart';
 
 class QuestionCard extends StatelessWidget {
   final QuestionPaperModel model;
@@ -10,32 +11,50 @@ class QuestionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: ColoredBox(
-                color: Theme.of(context).primaryColor.withOpacity(0.2),
-                child: SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: CachedNetworkImage(
-                    imageUrl: model.imageUrl!,
-                    placeholder: (context, url) => Container(
-                      alignment: Alignment.center,
-                      child: const CircularProgressIndicator(),
+    const double _padding = 10.0;
+    return Padding(
+      padding: const EdgeInsets.all(_padding),
+      child: Stack(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: ColoredBox(
+                  color: Theme.of(context).primaryColor.withOpacity(0.2),
+                  child: SizedBox(
+                    height: Get.width * 0.15,
+                    width: Get.width * 0.15,
+                    child: CachedNetworkImage(
+                      imageUrl: model.imageUrl!,
+                      placeholder: (context, url) => Container(
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          Image.asset("assets/images/app_splash_logo.png"),
                     ),
-                    errorWidget: (context, url, error) =>
-                        Image.asset("assets/images/app_splash_logo.png"),
                   ),
                 ),
               ),
-            )
-          ],
-        )
-      ],
+              const SizedBox(width: 12,),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(model.title),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 15),
+                      child: Text(model.description),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
