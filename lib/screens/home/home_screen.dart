cbe94d01ea14dc21389b5209086c6ba77a57d399
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-21 19:21:15 +0530

    ContentArea widget created

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
 import 'package:flutter_study_app/controller/question_papers/question_paper_controller.dart';
 import 'package:flutter_study_app/screens/home/question_card.dart';
+import 'package:flutter_study_app/widgets/content_area.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,19 +13,22 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     QuestionPaperController _questionPaperController = Get.find();
     return Scaffold(
-      body: Obx(() => ListView.separated(
-          padding: UIParameters.mobileScreenPadding,
-          itemBuilder: (BuildContext context, int index) {
-            return QuestionCard(
-              model: _questionPaperController.allPapers[index],
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(
-              height: 20,
-            );
-          },
-          itemCount: _questionPaperController.allPapers.length)),
+      body: ContentArea(
+        addPadding: false,
+        child: Obx(() => ListView.separated(
+            padding: UIParameters.mobileScreenPadding,
+            itemBuilder: (BuildContext context, int index) {
+              return QuestionCard(
+                model: _questionPaperController.allPapers[index],
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(
+                height: 20,
+              );
+            },
+            itemCount: _questionPaperController.allPapers.length)),
+      ),
     );
   }
 }
