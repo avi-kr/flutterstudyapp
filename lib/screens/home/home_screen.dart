b82c53d736512781b1c80a357ef5b36a4b0004e6
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-21 18:12:30 +0530

    image showing using cache network image

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_study_app/controller/question_papers/question_paper_controller.dart';
 import 'package:get/get.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,11 +17,14 @@ class HomeScreen extends StatelessWidget {
               child: SizedBox(
                 height: 200,
                 width: 20,
-                child: FadeInImage(
-                  image: NetworkImage(
-                      _questionPaperController.allPaperImages[index]),
-                  placeholder:
-                      const AssetImage("assets/images/app_splash_logo.png"),
+                child: CachedNetworkImage(
+                  imageUrl: _questionPaperController.allPapers[index].imageUrl!,
+                  placeholder: (context, url) => Container(
+                    alignment: Alignment.center,
+                    child: const CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) =>
+                      Image.asset("assets/images/app_splash_logo.png"),
                 ),
               ),
             );
@@ -30,7 +34,7 @@ class HomeScreen extends StatelessWidget {
               height: 20,
             );
           },
-          itemCount: _questionPaperController.allPaperImages.length)),
+          itemCount: _questionPaperController.allPapers.length)),
     );
   }
 }
