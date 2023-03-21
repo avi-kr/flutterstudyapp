e6d518fc27b4b52b6e79a0acaa6484e0dc031928
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-21 19:25:52 +0530

    question card widget modified

--- lib/configs/themes/app_dark_theme.dart
+++ lib/configs/themes/app_dark_theme.dart
@@ -6,6 +6,7 @@ import 'package:flutter_study_app/configs/themes/sub_theme_data_mixin.dart';
 const Color primaryLightColorLight = Color(0xFF3ac3cb);
 const Color primaryColorLight = Color(0xFFF85187);
 const Color mainTextColorDark = Colors.white;
+const Color cardColor = Color.fromARGB(255, 254, 254, 255);
 
 class DarkTheme with SubThemeData {
   buildDarkTheme() {
@@ -13,6 +14,7 @@ class DarkTheme with SubThemeData {
     return systemDarkTheme.copyWith(
         primaryColor: primaryColorLight,
         iconTheme: getIconTheme(),
+        cardColor: cardColor,
         textTheme: getTextTheme().apply(
             bodyColor: mainTextColorDark, displayColor: mainTextColorDark));
   }
--- lib/screens/home/question_card.dart
+++ lib/screens/home/question_card.dart
@@ -15,90 +15,96 @@ class QuestionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const double _padding = 10.0;
-    return Padding(
-      padding: const EdgeInsets.all(_padding),
-      child: Stack(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: ColoredBox(
-                  color: Theme.of(context).primaryColor.withOpacity(0.1),
-                  child: SizedBox(
-                    height: Get.width * 0.15,
-                    width: Get.width * 0.15,
-                    child: CachedNetworkImage(
-                      imageUrl: model.imageUrl!,
-                      placeholder: (context, url) => Container(
-                        alignment: Alignment.center,
-                        child: const CircularProgressIndicator(),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: UIParameters.cardBorderRadius,
+        color: Theme.of(context).cardColor
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(_padding),
+        child: Stack(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: ColoredBox(
+                    color: Theme.of(context).primaryColor.withOpacity(0.1),
+                    child: SizedBox(
+                      height: Get.width * 0.15,
+                      width: Get.width * 0.15,
+                      child: CachedNetworkImage(
+                        imageUrl: model.imageUrl!,
+                        placeholder: (context, url) => Container(
+                          alignment: Alignment.center,
+                          child: const CircularProgressIndicator(),
+                        ),
+                        errorWidget: (context, url, error) =>
+                            Image.asset("assets/images/app_splash_logo.png"),
                       ),
-                      errorWidget: (context, url, error) =>
-                          Image.asset("assets/images/app_splash_logo.png"),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      model.title,
-                      style: cartTitle(context),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10, bottom: 15),
-                      child: Text(model.description),
-                    ),
-                    Row(
-                      children: [
-                        AppIconText(
-                            icon: Icon(
-                              Icons.help_outline_sharp,
-                              color: Get.isDarkMode
-                                  ? Colors.white
-                                  : Theme.of(context).primaryColor,
-                            ),
-                            text: Text(
-                              '${model.questionCount} questions',
-                              style: detailText.copyWith(
-                                color: Get.isDarkMode
-                                    ? Colors.white
-                                    : Theme.of(context).primaryColor,
-                              ),
-                            )),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        AppIconText(
-                            icon: Icon(
-                              Icons.timer,
-                              color: Get.isDarkMode
-                                  ? Colors.white
-                                  : Theme.of(context).primaryColor,
-                            ),
-                            text: Text(
-                              model.timeInMinits(),
-                              style: detailText.copyWith(
-                                color: Get.isDarkMode
-                                    ? Colors.white
-                                    : Theme.of(context).primaryColor,
-                              ),
-                            ))
-                      ],
-                    )
-                  ],
+                const SizedBox(
+                  width: 12,
                 ),
-              )
-            ],
-          )
-        ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        model.title,
+                        style: cartTitle(context),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 15),
+                        child: Text(model.description),
+                      ),
+                      Row(
+                        children: [
+                          AppIconText(
+                              icon: Icon(
+                                Icons.help_outline_sharp,
+                                color: Get.isDarkMode
+                                    ? Colors.white
+                                    : Theme.of(context).primaryColor,
+                              ),
+                              text: Text(
+                                '${model.questionCount} questions',
+                                style: detailText.copyWith(
+                                  color: Get.isDarkMode
+                                      ? Colors.white
+                                      : Theme.of(context).primaryColor,
+                                ),
+                              )),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          AppIconText(
+                              icon: Icon(
+                                Icons.timer,
+                                color: Get.isDarkMode
+                                    ? Colors.white
+                                    : Theme.of(context).primaryColor,
+                              ),
+                              text: Text(
+                                model.timeInMinits(),
+                                style: detailText.copyWith(
+                                  color: Get.isDarkMode
+                                      ? Colors.white
+                                      : Theme.of(context).primaryColor,
+                                ),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
