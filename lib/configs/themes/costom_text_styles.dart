5d385c6d04c04777615b8880c2cbdf92550b36b6
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-23 16:25:21 +0530

    home upper constainer added

--- lib/configs/themes/costom_text_styles.dart
+++ lib/configs/themes/costom_text_styles.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_study_app/configs/themes/app_colors.dart';
 import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
 
 TextStyle cartTitle(context) => TextStyle(
@@ -11,3 +12,5 @@ TextStyle cartTitle(context) => TextStyle(
     fontWeight: FontWeight.bold);
 
 const detailText = TextStyle(fontSize: 12);
+const headerText = TextStyle(
+    fontSize: 22, fontWeight: FontWeight.w700, color: onSurfaceTextColor);
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_study_app/app_icons.dart';
+import 'package:flutter_study_app/configs/themes/app_colors.dart';
+import 'package:flutter_study_app/configs/themes/costom_text_styles.dart';
 import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
 import 'package:flutter_study_app/controller/question_papers/question_paper_controller.dart';
 import 'package:flutter_study_app/screens/home/question_card.dart';
@@ -13,21 +16,65 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     QuestionPaperController _questionPaperController = Get.find();
     return Scaffold(
-      body: ContentArea(
-        addPadding: false,
-        child: Obx(() => ListView.separated(
-            padding: UIParameters.mobileScreenPadding,
-            itemBuilder: (BuildContext context, int index) {
-              return QuestionCard(
-                model: _questionPaperController.allPapers[index],
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return const SizedBox(
-                height: 20,
-              );
-            },
-            itemCount: _questionPaperController.allPapers.length)),
+      body: Container(
+        decoration: BoxDecoration(gradient: mainGradient()),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(mobileScreenPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Icon(
+                      AppIcons.menuLeft,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            AppIcons.peace,
+                          ),
+                          Text(
+                            "hello Frinds",
+                            style: detailText.copyWith(color: onSurfaceTextColor),
+                          )
+                        ],
+                      ),
+                    ),
+                    Text("What do you want to learn today?", style: headerText,)
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ContentArea(
+                    addPadding: false,
+                    child: Obx(() => ListView.separated(
+                        padding: UIParameters.mobileScreenPadding,
+                        itemBuilder: (BuildContext context, int index) {
+                          return QuestionCard(
+                            model: _questionPaperController.allPapers[index],
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const SizedBox(
+                            height: 20,
+                          );
+                        },
+                        itemCount: _questionPaperController.allPapers.length)),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
