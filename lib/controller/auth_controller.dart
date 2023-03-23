b3826b0800c22efce19be79cca1e8381076579e6
Abhishek Kumar <avi.kr16@gmail.com>
2023-03-23 17:17:29 +0530

    firebase auth instance, state and user rx funtion added in auth controller

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController extends GetxController {
   @override
@@ -7,8 +8,17 @@ class AuthController extends GetxController {
     super.onReady();
   }
 
+  late FirebaseAuth _auth;
+  late Stream<User?> _authStateChanges;
+  final _user = Rxn<User>();
+
   void initAuth() async {
     await Future.delayed(const Duration(seconds: 2));
+    _auth = FirebaseAuth.instance;
+    _authStateChanges = _auth.authStateChanges();
+    _authStateChanges.listen((User? user) {
+      _user.value = user;
+    });
     navigateToIntroduction();
   }
 
