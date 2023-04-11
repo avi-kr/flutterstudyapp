8230e5811ed9f8e507705720c5a33549c4bab2de
Abhishek Kumar <avi.kr16@gmail.com>
2023-04-11 18:56:37 +0530

    creating answer widget; selected anser color and border answer color created

--- lib/configs/themes/app_colors.dart
+++ lib/configs/themes/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study_app/configs/themes/app_dark_theme.dart';
 import 'package:flutter_study_app/configs/themes/app_light_theme.dart';
 import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
+import 'package:get/get.dart';
 
 const Color onSurfaceTextColor = Colors.white;
 
@@ -24,3 +25,11 @@ LinearGradient mainGradient() =>
 Color customScaffoldColor(BuildContext context) => UIParameters.isDarkMode()
     ? const Color(0xFF2e3c62)
     : const Color.fromARGB(255, 240, 237, 255);
+
+Color answerSelectedColor() => UIParameters.isDarkMode()
+    ? Theme.of(Get.context!).cardColor.withOpacity(0.5)
+    : Theme.of(Get.context!).primaryColor;
+
+Color answerBorderColor() => UIParameters.isDarkMode()
+    ? const Color.fromARGB(255, 20, 46, 158)
+    : const Color.fromARGB(255, 221, 221, 221);
